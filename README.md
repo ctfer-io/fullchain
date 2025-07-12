68c1dfd532dc9c82408fdf58bc9a5bba09ae242c
Nicolas FAUGEROUX <100589021+NicoFgrx@users.noreply.github.com>
2025-07-12 21:49:34 +0200

    Alpha release (#5)
    * chore: clean repo and docs
    
    * fix: use chall-manager v0.4.5
    
    * feat: add network policies for CTFd -> CM traffic
    
    * fix: netpol ctfd-cm
    
    * fix: re-use podLabels
    
    * fix: namespace using random string is stuck
    
    * docs: add missing configuration for metallb
    
    * impr: use ctfer v0.1.0 instead of hash
    
    * fix: typo
    
    * fix: default storageClass
    
    ---------
    
    Co-authored-by: Lucas TESSON <lucastesson@protonmail.com>

--- README.md
+++ README.md
@@ -1,19 +1,172 @@
-## Dev setup
+<div align="center">
+  <h1>Fullchain</h1>
+  <a href=""><img src="https://img.shields.io/github/license/ctfer-io/fullchain?style=for-the-badge" alt="License"></a>
+</div>
+
+The *Fullchain* component helps you deploy a ready-to-use CTF (Capture The Flag) platform with [CTFd](https://github.com/ctfd/ctfd), [Chall-Manager](https://github.com/ctfer-io/chall-manager), and the [CTFd-chall-manager](https://github.com/ctfer-io/ctfd-chall-manager) plugins already configured.
+
+
+> [!CAUTION]
+>
+> This component is an **internal** work mostly used for development purposes.
+> It is used for production purposes too, i.e. on Capture The Flag events.
+>
+> Nonetheless, **we do not include it in the repositories we are actively maintaining**.
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Advanced Setup](#advanced-setup)
+
+## Getting Started
+
+To get started with the Fullchain project and deploy it inside your cluster, follow these steps:
+
+For now, the `github.com/ctfer-io/ctfer` dependency is **private** and needs some tricks to use it.
+
+1. **Set up SSH Agent and Add SSH Key:**
 
 ```bash
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-export GOPRIVATE=github.com/ctfer-io/monitoring,github.com/ctfer-io/ctfer
+export GOPRIVATE=github.com/ctfer-io/ctfer
+```
+
+2. **Download all dependencies:**
+
+```bash
 go mod tidy
+```
 
-# Start Pulumi program
-export KUBECONFIG="~/.kube/config-dev1"
-export PULUMI_CONFIG_PASSPHRASE=""
+3. **Initialize Pulumi Stack:**
+
+```bash
+pulumi login --local
 pulumi stack init prod
-pulumi config set --secret chall-kube-config "$(cat ~/.kube/config-dev2)"
+```
 
-cat ~/ctfer-io/ctfer/certs/ctfd.crt | pulumi config set --secret ctfd-crt
-cat ~/ctfer-io/ctfer/certs/ctfd.key | pulumi config set --secret ctfd-key
+4. **Configure Dedicated Challenges Cluster (Optional):**
 
+If you want to configure a dedicated cluster for challenges, run:
+
+```bash
+pulumi config set --secret chall-kube-config "$(cat ~/.kube/config-challenge)"
+```
+
+5. **Set CTFd Certificates:**
+
+```bash
+export PULUMI_CONFIG_PASSPHRASE="xx"
+cat path/to/ctfd.crt | pulumi config set --secret ctfd-crt
+cat path/to/ctfd.key | pulumi config set --secret ctfd-key
+```
+
+6. **Set CTFd URL:**
+
+```bash
+pulumi config set ctfd-hostname ctfd.yourdomain
+```
+
+6. **Deploy the Stack:**
+
+```bash
 pulumi up -y
+```
+
+## Advanced Setup
+
+### Air-Gap Environment
+
+For air-gap environments, you need to download all images and upload them into your registry before deployment. You can use [Hauler](https://docs.hauler.dev/) to download and push all images at once.
+
+The following actions must be performed before the `pulumi up -y`.
+
+1. **Navigate to the Hack Directory:**
+
+```bash
+cd hack
+```
+
+2. **Sync Images with Hauler:**
+
+```bash
+hauler store sync -f chaine-totale.yml
+```
+
+3. **Copy Images to Your Registry:**
+
+```bash
+hauler store copy registry://your-registry:5000
+```
+
+4. **Configure the Registry to Use on Your Stack:**
+
+```bash
+pulumi config set registry your-registry:5000
+```
+
+### Without CTFer-L3
+
+If you are not using the [L3](https://github.com/ctfer-io/ctfer), you need to install some Helm charts manually:
+- [Longhorn](https://longhorn.io/): to enable persistent storage on chall-manager, ctfd's database... ;
+- [Traefik](https://traefik.io): as ingress controller to route HTTPS traffic from outside ;
+- [Cilium](https://docs.cilium.io/): as internal CNI;
+- [MetalLB](https://metallb.io/): as Load Balancer used by Traefik.
+
+The following commands can be different depending on your Kubernetes setup (if you are using Talos based cluster):
+
+```bash
+# Install Cilium CNI
+helm repo add cilium https://helm.cilium.io/
+helm repo update
+helm install cilium cilium/cilium --version 1.17.5 --namespace kube-system
+
+# Install Longhorn
+helm repo add longhorn https://charts.longhorn.io
+helm repo update
+helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.9.0
+
+# Install MetalLB
+helm repo add metallb https://metallb.github.io/metallb
+helm install metallb metallb/metallb --namespace metallb-system --set speaker.frr.enabled=false --create-namespace --version 0.14.9
+
+# Configure Metallb addresses pool EDIT as your need
+cat <<EOF > ipaddresspool.yml
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: loadbalancer-pool
+  namespace: metallb-system
+spec:
+  addresses:
+  - 10.17.12.200/32
+  autoAssign: true
+EOF
+
+kubectl apply -f ipaddresspool.yml
+
+# Install Traefik
+helm repo add traefik https://traefik.github.io/charts
+helm repo update
+
+cat <<EOF > traefik-values.yml
+ports:
+  web:
+    redirections:
+      entryPoint:
+        scheme: https
+        to: websecure
+  websecure:
+    asDefault: true
+providers:
+  kubernetesCRD:
+    enabled: true
+  kubernetesIngress:
+    allowCrossNamespace: true
+EOF
+
+helm install traefik traefik/traefik --namespace ingress-controller --create-namespace --version 35.2.0 -f traefik-values.yml
+
+
+
 ```
--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
+	netwv1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/networking/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+
+	// romeoenv "github.com/ctfer-io/romeo/environment/deploy/parts"
 
 	"github.com/ctfer-io/chall-manager/deploy/common"
 	challmanager "github.com/ctfer-io/chall-manager/deploy/services"
 	ctfer "github.com/ctfer-io/ctfer/services"
+	"github.com/ctfer-io/fullchain/parts"
 	monitoring "github.com/ctfer-io/monitoring/services"
-	romeoenv "github.com/ctfer-io/romeo/environment/deploy/parts"
 )
 
 func main() {
@@ -24,76 +32,166 @@ func main() {
 		opts := []pulumi.ResourceOption{}
 
 		// => Monitoring
-		mon, err := monitoring.NewMonitoring(ctx, "monitoring", &monitoring.MonitoringArgs{
-			ColdExtract: cfg.ColdExtract,
-			Registry:    pulumi.String("registry.dev1.ctfer-io.lab"),
-		}, opts...)
+		monConf := &monitoring.MonitoringArgs{
+			ColdExtract:      cfg.ColdExtract,
+			StorageClassName: pulumi.String("longhorn"),
+		}
+		if cfg.Registry != "" {
+			monConf.Registry = pulumi.String(cfg.Registry)
+		}
+		mon, err := monitoring.NewMonitoring(ctx, "monitoring", monConf, opts...)
 		if err != nil {
 			return err
 		}
 
 		// => Namespace to deploy the platform
-		ns, err := corev1.NewNamespace(ctx, "ctf", &corev1.NamespaceArgs{}, opts...)
-		if err != nil {
-			return err
-		}
-
-		// => Romeo env
-		renv, err := romeoenv.NewRomeoEnvironment(ctx, "romeo-env", &romeoenv.RomeoEnvironmentArgs{
-			Namespace: ns.Metadata.Name().Elem(),
-			Tag:       pulumi.String("24hiut2025"),
-			Registry:  pulumi.String("registry.dev1.ctfer-io.lab"),
-			PVCAccessModes: pulumi.ToStringArray([]string{
-				"ReadWriteMany",
-			}),
+		ns, err := parts.NewNamespace(ctx, "ctf", &parts.NamespaceArgs{
+			Name: pulumi.String("fullchain"),
 		}, opts...)
 		if err != nil {
 			return err
 		}
 
 		// => Chall-Manager
-		ch, err := challmanager.NewChallManager(ctx, "chall-manager", &challmanager.ChallManagerArgs{
+		cmConf := &challmanager.ChallManagerArgs{
 			LogLevel: pulumi.String("info"),
 			Requests: pulumi.ToStringMap(map[string]string{
 				"memory": "2Gi",
 				"cpu":    "1.0",
 			}),
-			PVCStorageSize: pulumi.String("50Gi"),
-			Kubeconfig:     cfg.ChallKubeConfig,
-			Tag:            pulumi.String("v0.4.4"),
-			Registry:       pulumi.String("registry.dev1.ctfer-io.lab"),
-			Namespace:      ns.Metadata.Name().Elem(),
+			PVCStorageSize: pulumi.String("10Gi"),
+			Tag:            pulumi.String("v0.4.5"),
+
+			Namespace: ns.Name,
 			Otel: &common.OtelArgs{
 				Endpoint:    mon.OTEL.Endpoint,
-				ServiceName: pulumi.String("24hiut2025"),
+				ServiceName: pulumi.String(ctx.Stack()),
 				Insecure:    true, // XXX @pandatix fix this shit
 			},
-			RomeoClaimName: renv.ClaimName,
-		}, opts...)
+		}
+
+		if cfg.Registry != "" {
+			cmConf.Registry = pulumi.String(cfg.Registry)
+		}
+		cm, err := challmanager.NewChallManager(ctx, "chall-manager", cmConf, opts...)
 		if err != nil {
 			return err
 		}
 
-		// => CTFer/CTFd
-		ctfer, err := ctfer.NewCTFer(ctx, "platform", &ctfer.CTFerArgs{
-			Namespace:        ns.Metadata.Name().Elem(),
-			Hostname:         pulumi.String("ctfd.24hiut2025.ctfer.io"),
-			CTFdImage:        pulumi.String("ctferio/ctfd:3.7.7-0.3.4"),
-			CTFdCrt:          cfg.CTFdCrt,
-			CTFdKey:          cfg.CTFdKey,
-			CTFdStorageSize:  pulumi.String("30Gi"),
-			ChartsRepository: pulumi.String("oci://registry.dev1.ctfer-io.lab/hauler"),
-			ImagesRepository: pulumi.String("registry.dev1.ctfer-io.lab"),
-			ChallManagerUrl:  pulumi.Sprintf("http://%s/api/v1", ch.Endpoint),
-			CTFdWorkers:      pulumi.Int(1),
-			CTFdReplicas:     pulumi.Int(1),
-		}, opts...)
+		// // => CTFer/CTFd
+		ctfdConf := &ctfer.CTFerArgs{
+			Namespace:       ns.Name,
+			Hostname:        cfg.CTFdHostname,
+			CTFdImage:       pulumi.String("ctferio/ctfd:3.7.7-0.3.4"),
+			CTFdCrt:         cfg.CTFdCrt,
+			CTFdKey:         cfg.CTFdKey,
+			CTFdStorageSize: pulumi.String("10Gi"),
+			ChallManagerUrl: pulumi.Sprintf("http://%s/api/v1", cm.Endpoint),
+
+			// => Ingress-related configuration
+			IngressNamespace: pulumi.String("ingress-controller"),
+			IngressLabels: pulumi.ToStringMap(map[string]string{
+				"app.kubernetes.io/name": "traefik",
+			}),
+		}
+
+		// Air-gapped
+		if cfg.Registry != "" {
+			ctfdConf.ImagesRepository = pulumi.String(cfg.Registry)
+			ctfdConf.ChartsRepository = pulumi.Sprintf("oci://%s/hauler", cfg.Registry)
+		}
+
+		ctfer, err := ctfer.NewCTFer(ctx, "platform", ctfdConf, opts...)
 		if err != nil {
 			return err
 		}
 
-		ctx.Export("namespace", ns.Metadata.Name().Elem())
-		ctx.Export("romeo-claim-name", renv.ClaimName)
+		if _, err := netwv1.NewNetworkPolicy(ctx, "ctfd-to-cm", &netwv1.NetworkPolicyArgs{
+			Metadata: metav1.ObjectMetaArgs{
+				Namespace: ns.Name,
+				Labels: pulumi.StringMap{
+					"app.kubernetes.io/part-of": pulumi.String("fullchain"),
+					"ctfer.io/stack-name":       pulumi.String(ctx.Stack()),
+				},
+			},
+			Spec: netwv1.NetworkPolicySpecArgs{
+				PolicyTypes: pulumi.ToStringArray([]string{
+					"Egress",
+				}),
+				PodSelector: metav1.LabelSelectorArgs{
+					MatchLabels: ctfer.PodLabels,
+				},
+				Egress: netwv1.NetworkPolicyEgressRuleArray{
+					netwv1.NetworkPolicyEgressRuleArgs{
+						To: netwv1.NetworkPolicyPeerArray{
+							netwv1.NetworkPolicyPeerArgs{
+								NamespaceSelector: metav1.LabelSelectorArgs{
+									MatchLabels: pulumi.StringMap{
+										"kubernetes.io/metadata.name": ns.Name,
+									},
+								},
+								PodSelector: metav1.LabelSelectorArgs{
+									MatchLabels: cm.PodLabels,
+								},
+							},
+						},
+						Ports: netwv1.NetworkPolicyPortArray{
+							netwv1.NetworkPolicyPortArgs{
+								Port:     parsePort(cm.Endpoint),
+								Protocol: pulumi.String("TCP"),
+							},
+						},
+					},
+				},
+			},
+		}, opts...); err != nil {
+			return err
+		}
+
+		if _, err := netwv1.NewNetworkPolicy(ctx, "cm-from-ctfd", &netwv1.NetworkPolicyArgs{
+			Metadata: metav1.ObjectMetaArgs{
+				Namespace: ns.Name,
+				Labels: pulumi.StringMap{
+					"app.kubernetes.io/part-of": pulumi.String("fullchain"),
+					"ctfer.io/stack-name":       pulumi.String(ctx.Stack()),
+				},
+			},
+			Spec: netwv1.NetworkPolicySpecArgs{
+				PolicyTypes: pulumi.ToStringArray([]string{
+					"Ingress",
+				}),
+				PodSelector: metav1.LabelSelectorArgs{
+					MatchLabels: cm.PodLabels,
+				},
+				Ingress: netwv1.NetworkPolicyIngressRuleArray{
+					netwv1.NetworkPolicyIngressRuleArgs{
+						From: netwv1.NetworkPolicyPeerArray{
+							netwv1.NetworkPolicyPeerArgs{
+								NamespaceSelector: metav1.LabelSelectorArgs{
+									MatchLabels: pulumi.StringMap{
+										"kubernetes.io/metadata.name": ns.Name,
+									},
+								},
+								PodSelector: metav1.LabelSelectorArgs{
+									MatchLabels: ctfer.PodLabels,
+								},
+							},
+						},
+						Ports: netwv1.NetworkPolicyPortArray{
+							netwv1.NetworkPolicyPortArgs{
+								Port:     parsePort(cm.Endpoint),
+								Protocol: pulumi.String("TCP"),
+							},
+						},
+					},
+				},
+			},
+		}, opts...); err != nil {
+			return err
+		}
+
+		ctx.Export("namespace", ns.Name)
+		ctx.Export("chall-manager-endpoint", cm.Endpoint)
 		ctx.Export("url", ctfer.URL)
 		return nil
 	})
@@ -104,14 +202,46 @@ type Config struct {
 	ChallKubeConfig pulumi.StringInput
 	CTFdCrt         pulumi.StringInput
 	CTFdKey         pulumi.StringInput
+	CTFdHostname    pulumi.StringInput
+	Registry        string
 }
 
 func InitConfig(ctx *pulumi.Context) (*Config, error) {
 	cfg := config.New(ctx, "")
 	return &Config{
-		ColdExtract:     cfg.GetBool("cold-extract"),
-		ChallKubeConfig: cfg.GetSecret("chall-kube-config"),
-		CTFdCrt:         cfg.GetSecret("ctfd-crt"),
-		CTFdKey:         cfg.GetSecret("ctfd-key"),
+		ColdExtract:  cfg.GetBool("cold-extract"),
+		CTFdCrt:      cfg.RequireSecret("ctfd-crt"),
+		CTFdKey:      cfg.RequireSecret("ctfd-key"),
+		CTFdHostname: pulumi.String(cfg.Require("ctfd-hostname")),
+		Registry:     cfg.Get("registry"),
 	}, nil
+}
+
+// parsePort cuts the input endpoint to return its port.
+// Example: some.thing:port -> port
+func parsePort(edp pulumi.StringInput) pulumi.IntOutput {
+	return edp.ToStringOutput().ApplyT(func(edp string) (int, error) {
+		_, pStr, _ := strings.Cut(edp, ":")
+		p, err := strconv.Atoi(pStr)
+		if err != nil {
+			return 0, errors.Wrapf(err, "parsing endpoint %s for port", edp)
+		}
+		return p, nil
+	}).(pulumi.IntOutput)
+}
+
+// parseURLPort parses the input endpoint formatted as a URL to return its port.
+// Example: http://some.thing:port -> port
+func parseURLPort(edp pulumi.StringOutput) pulumi.IntOutput {
+	return edp.ToStringOutput().ApplyT(func(edp string) (int, error) {
+		u, err := url.Parse(edp)
+		if err != nil {
+			return 0, errors.Wrapf(err, "parsing endpoint %s as a URL", edp)
+		}
+		p, err := strconv.Atoi(u.Port())
+		if err != nil {
+			return 0, errors.Wrapf(err, "parsing endpoint %s for port", edp)
+		}
+		return p, nil
+	}).(pulumi.IntOutput)
 }
